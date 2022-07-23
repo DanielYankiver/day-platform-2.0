d22af0d0df60387add8a34f9ccee6b97d7c7f83e
Daniel Yankiver <69213153+DanielYankiver@users.noreply.github.com>
2022-07-23 08:56:26 -0400

    added github logo to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import './Footer.css'
+import github from '../images/github-logo.svg'
 
 export default function Footer() {
 
   return (
     <div className='footer-container'>
-    <div className='footer-title'>
+    {/* <div className='footer-title'>
       Footer
+    </div> */}
+      <img
+        className='github-logo'
+        src={github}
+      />
     </div>
-  </div>
   )
 }
