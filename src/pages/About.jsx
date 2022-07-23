dfd6639b32246aabef0b8469b94830977d1e476b
Daniel Yankiver <69213153+DanielYankiver@users.noreply.github.com>
2022-07-23 12:57:36 -0400

    made name ascii and moved about to about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,8 +4,11 @@ import './About.css';
 function About() {
     return (
       <div className='about-container'>
-        <div className='about-title'>
-          About Page
+        <div className='intro'>
+          Creative software engineer with a background in filmmaking. I have experience building applications with Javascript, React.js, CSS, Next.js, GraphQL and HTML. I'm a full-stack web developer with projects that incorporate frontend and backend development with robust user interfaces (UI) that use efficient code to solve real-world problems.
+        </div>
+        <div className='skills'>
+          LIST ALL: <br/> Languages, Frameworks, Libraries, Tools & Skills
         </div>
       </div> 
     );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,11 +13,13 @@ function Home() {
               src={logo}
             />
           </div>
-          <div className='home-title'>DANIEL YANKIVER</div>
-          <div className='home-intro-container'>
-            <div className='home-intro'>
-            Creative software engineer with a background in filmmaking. I have experience building applications with Javascript, React.js, CSS, Next.js, GraphQL and HTML. I'm a full-stack web developer with projects that incorporate frontend and backend development with robust user interfaces (UI) that use efficient code to solve real-world problems.
-            </div>
+          <div className='first-name'>
+            █▀▄ ▄▀█ █▄░█ █ █▀▀ █░░
+            █▄▀ █▀█ █░▀█ █ ██▄ █▄▄
+          </div>
+          <div className='last-name'>
+            █▄█ ▄▀█ █▄░█ █▄▀ █ █░█ █▀▀ █▀█
+            ░█░ █▀█ █░▀█ █░█ █ ▀▄▀ ██▄ █▀▄
           </div>
           <div className='ascii-button-container'>
             <a href="https://www.linkedin.com/in/daniel-yankiver/" target="_blank" rel="noopener noreferrer">
