712971c2033ba984efd1535f58802f2fd8da6c9a
Daniel Yankiver <69213153+DanielYankiver@users.noreply.github.com>
2022-08-06 17:15:10 -0400

    adding floppy image with pure css

--- src/pages/SoftwareEngineer.jsx
+++ src/pages/SoftwareEngineer.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+// import floppy from '../images/floppy-disk.svg'
 import './SoftwareEngineer.css'
 
 function SoftwareEngineer() {
     return (
       <div className='software-engineer-container'>
-        <div className='software-engineer-title'>
+        {/* <div className='software-engineer-title'>
           Software Engineer Page
+        </div> */}
+        {/* <img href={floppy} className='floppy'/> */}
+        <div className='floppy-background'>
+
         </div>
       </div> 
     );
