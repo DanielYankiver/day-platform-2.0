2ed227c88e48a9a80e71dc06260f724b1493c06f
Daniel Yankiver <69213153+DanielYankiver@users.noreply.github.com>
2022-11-03 18:43:42 -0400

    cleaned up the filmmaker page some more

--- src/pages/Filmmaker.jsx
+++ src/pages/Filmmaker.jsx
@@ -5,9 +5,9 @@ import './Filmmaker.css'
 function Filmmaker() {
     return (
       <div className='filmmaker-container'>
-        <div className='filmmaker-title'>
-          Showreel
-        </div>
+        {/* <div className='filmmaker-title'>
+          SHOWREEL
+        </div> */}
         <div className='showreel'>
           <iframe src="https://player.vimeo.com/video/375538495?h=e5986dfa91"  frameBorder="0" allow="autoplay; fullscreen; picture-in-picture" allowFullScreen></iframe>
         </div>
